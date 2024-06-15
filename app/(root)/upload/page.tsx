8b4f5be835b88158f8ed4d606040571a87810ba9
harkanni <harkanzyempire001@gmail.com>
2024-06-15 08:23:12 +0100

    uploaad page

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,4 +1,4 @@
-'use clientSide'
+'use client'
 
 import { avatar_01, avatar_02, avatar_03, avatar_04, avatar_05 } from '@/assets'
 import Preview from '@/component/Preview'
@@ -14,7 +14,7 @@ const page = () => {
 
    const handlePreview = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
       setContent(event.target.value);
-    };
+   };
 
    return (
       <>
@@ -81,15 +81,23 @@ const page = () => {
                               </li>
                            </ul>
                         </div>
-                        <h1 className="cpynq cjd5a ciz93" style={{marginBottom: "1.5rem"}}>Start typing your post.
+                        <h1 className="cpynq cjd5a ciz93" style={{ marginBottom: "1.5rem" }}>Start typing your post.
                            Preview appears below</h1>
                      </header>
                   </article>
 
                   <Preview value={content} />
+                  {/* 
+                     <pre class="cgr7g c2bb0 ca9r6 cx6ng c9xwx cme8e cmy5q">
+                        <code style="border-radius: 0.5rem">var str = "Hello world"
+                           const greet = () => {console.log(str)}
+                        </code>
+                     </pre>
+                  */}
 
-                  <form style={{display: 'block', width: '100%'}} id="content-form">
-                     <input type="text" className="upload-author" placeholder="Enter Title" id="upload-title" />
+
+                  <form style={{ display: 'block', width: '100%' }} id="content-form">
+                     <input type="text" className="upload-author upload-title" placeholder="Enter Title" id="upload-title" />
                      <textarea name="" id="upload-area" className="upload-area" placeholder="start typing...."
                         onInput={handlePreview} value={content}></textarea>
                      <textarea typeof='text' className="upload-description" placeholder="Enter your description" id="upload-description"></textarea>
@@ -147,7 +155,7 @@ const page = () => {
                   <div className="cg3vi cj965 cry3b c81t6 cax03 c1gvt czh94 czz51 c90wr c5oxi">
                      <div className="c5a0p ckgwf c4udf cfwvb">
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="20">
-                           <path fill="#38BDF8" fill-rule="evenodd"
+                           <path fill="#38BDF8" fillRule="evenodd"
                               d="M.73 6.173a9.92 9.92 0 0 1 3.527-4.488A9.294 9.294 0 0 1 9.58 0h.737v4.67l.14-.226a9.68 9.68 0 0 1 4.3-3.683A9.205 9.205 0 0 1 20.29.192a9.461 9.461 0 0 1 4.904 2.737 10.143 10.143 0 0 1 2.622 5.12c.37 1.94.18 3.95-.545 5.778a9.92 9.92 0 0 1-3.528 4.488A9.294 9.294 0 0 1 18.42 20h-.737v-4.67a10.459 10.459 0 0 1-.14.226 9.68 9.68 0 0 1-4.3 3.683 9.205 9.205 0 0 1-5.534.569 9.461 9.461 0 0 1-4.904-2.737 10.143 10.143 0 0 1-2.622-5.12C-.186 10.01.004 8 .73 6.173ZM8.841 10V1.573a7.89 7.89 0 0 0-3.766 1.391A8.394 8.394 0 0 0 2.09 6.762a8.808 8.808 0 0 0-.462 4.889 8.583 8.583 0 0 0 2.219 4.332 8.006 8.006 0 0 0 4.15 2.316 7.789 7.789 0 0 0 4.683-.482 8.18 8.18 0 0 0 3.528-2.95 4.958 4.958 0 0 1-2.209.518c-2.849 0-5.158-2.411-5.158-5.385Zm10.316 8.427a7.89 7.89 0 0 0 3.766-1.391 8.393 8.393 0 0 0 2.985-3.798 8.807 8.807 0 0 0 .462-4.889 8.583 8.583 0 0 0-2.219-4.332 8.006 8.006 0 0 0-4.15-2.316 7.789 7.789 0 0 0-4.683.482 8.179 8.179 0 0 0-3.528 2.95A4.958 4.958 0 0 1 14 4.615c2.849 0 5.158 2.411 5.158 5.385v8.427Z">
                            </path>
                         </svg>
